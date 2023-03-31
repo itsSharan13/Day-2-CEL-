ba9391570e9279cbdf5d2fe92c4e86b224c792ed
itsSharan13 <108917162+itsSharan13@users.noreply.github.com>
2023-03-31 10:06:19 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navapp from './day 2/Navapp';
+import Footer from './day 2/Footer';
+import Image from './day 2/Image';
+function App() {
+  return (
+    <div>
+       <Navapp></Navapp>
+       <Image/>
+       <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Footer.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Facebook, Instagram, Twitter } from '@mui/icons-material';
+
+const Footer =()=>
+{
+    return<footer><div className='footer'>
+        <Facebook/>
+        <Instagram/>
+        <Twitter/>
+        <h6>Contact Details : 0001-5423-6798</h6>
+        </div>
+    </footer>
+}
+export default Footer
